3b5db042ca9af0a732c3276f4e4c71b179469428
xuli2020sj <72131412+xuli2020sj@users.noreply.github.com>
2022-06-04 21:39:45 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,17 @@
 #include <QApplication>
 #include <QPushButton>
 #include "mainwindow.h"
+#include "spdlog/spdlog.h"
+#include <Eigen/Dense>
+
 
 int main(int argc, char *argv[]) {
+    Eigen::MatrixXd m(2,2);
+
     QApplication a(argc, argv);
+
     MainWindow mw;
     mw.show();
+    spdlog::info("hello");
     return QApplication::exec();
 }
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -6,11 +6,13 @@
 
 #include "mainwindow.h"
 #include "ui_MainWindow.h"
-
+#include "spdlog/spdlog.h"
 
 MainWindow::MainWindow(QWidget *parent) :
         QWidget(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+
+    spdlog::error("Some error message with arg: {}", 1);
 }
 
 MainWindow::~MainWindow() {
